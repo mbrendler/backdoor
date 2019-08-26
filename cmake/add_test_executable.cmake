318bc3cf6097a7a352ca84bdbd39dab9e2297f24
Maik Brendler <maik.brendler@injixo.com>
2019-08-26 19:43:07 +0200

    Add cmock test command to cmake

---
+++ cmake/add_test_executable.cmake
@@ -0,0 +1,12 @@
+
+function(add_test_executable target)
+  add_executable(${target} ${ARGN})
+  target_link_libraries(${target} cmocka)
+  add_test(${target} ${target})
+endfunction()
+
+function(target_wrap_function target)
+  foreach(fn IN LISTS ARGN)
+    target_link_options("${target}" PUBLIC "-Wl--wrap,${fn}")
+  endforeach()
+endfunction()
