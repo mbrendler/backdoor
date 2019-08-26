3998f0a9203befd020ac4a645dfb1a93ef58e812
Maik Brendler <maik.brendler@injixo.com>
2019-08-26 19:43:53 +0200

    Test lib functions

---
+++ test/test.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <string.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+# include <cmocka.h>
+#pragma clang diagnostic pop
---
+++ test/test_lib.c
@@ -0,0 +1,56 @@
+#include "../src/lib.h"
+#include "test.h"
+#include <stdlib.h>
+
+static void test_parse_port_number(void** state) {
+  (void)state;
+  assert_int_equal(123, parse_port_number("123"));
+  assert_int_equal(1, parse_port_number("1"));
+  assert_int_equal(65535, parse_port_number("65535"));
+  assert_int_equal(-1, parse_port_number("65536"));
+  assert_int_equal(-1, parse_port_number("0"));
+  assert_int_equal(-1, parse_port_number("12a"));
+  assert_int_equal(-1, parse_port_number("a"));
+  assert_int_equal(-1, parse_port_number(""));
+}
+
+static void test_str_isnumber(void** state) {
+  (void)state;
+  assert_true(str_isnumber("123"));
+  assert_false(str_isnumber("12 3"));
+  assert_false(str_isnumber("123a"));
+  assert_false(str_isnumber("a"));
+}
+
+static void test_split_filename_and_number(void** state) {
+  (void)state;
+  char* number = NULL;
+
+  char* file1 = strdup(__FILE__ ":123");
+  split_filename_and_number(file1, &number);
+  assert_string_equal(__FILE__, file1);
+  assert_string_equal("123", number);
+  free(file1);
+
+  char* file2 = strdup(__FILE__);
+  number = NULL;
+  split_filename_and_number(file2, &number);
+  assert_string_equal(__FILE__, file2);
+  assert_null(number);
+  free(file2);
+
+  char* file3 = strdup(__FILE__ ":");
+  split_filename_and_number(file3, &number);
+  assert_string_equal(__FILE__, file3);
+  assert_null(number);
+  free(file3);
+}
+
+int main() {
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_parse_port_number),
+    cmocka_unit_test(test_str_isnumber),
+    cmocka_unit_test(test_split_filename_and_number),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
