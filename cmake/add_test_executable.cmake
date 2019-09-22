1ce9f1cf975aa935b325796b802412c7a691713c
Maik Brendler <maik.brendler@injixo.com>
2019-09-22 14:43:34 +0200

    Extend compile options for tests

--- cmake/add_test_executable.cmake
+++ cmake/add_test_executable.cmake
@@ -2,6 +2,12 @@
 function(add_test_executable target)
   add_executable(${target} ${ARGN})
   target_link_libraries(${target} cmocka)
+  set_target_properties(${target} PROPERTIES
+    INTERPROCEDURAL_OPTIMIZATION FALSE
+    COMPILE_DEFINITIONS TEST
+    # -fno-builtin is needed to mock exit calls
+    COMPILE_FLAGS "-g -O0 -fno-builtin"
+  )
   add_test(${target} ${target})
 endfunction()
 
